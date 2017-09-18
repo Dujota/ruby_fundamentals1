80f99c60eb4b3a1422751b94459aa6a2474f0590
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-18 13:49:17 -0400

    added boolean comparisons

--- exercise1.rb
+++ exercise1.rb
@@ -85,3 +85,20 @@ puts "Hello\t\tworld"
 puts "Hello\b\b\b\b\bGoodbye world"
 puts "Hello\rStart over world"
 puts "1. Hello\n2. World"
+
+
+# Boolean comparison
+puts (1 < 3) && (3 < 5)
+# true
+
+puts (1 > 1) && (2 > 2)
+# false
+
+puts (1 == 2) || (2 < 3)
+# true
+
+puts (1 != 2) || (2 < 3)
+# true
+
+puts (1 == 2) || (2 == 3)
+#false
