c4fbf312e4589faf94ceb935f2773dc73a2428e0
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-18 14:57:03 -0400

    added some samples for variables and boolean logic

--- excercise2.rb
+++ excercise2.rb
@@ -17,4 +17,32 @@ puts "the result of 45628 multiplied by 7839 is #{45628*7839}"
 
 puts (10 < 20 && 30 < 20) || !(10 == 11)
 
-# Part 2 b-b
+# Part 2 b
+
+# To store a number or a string in your computer's
+# memory for use later in your program, you need to assign it to a variable,
+# like so:
+# my_variable = 'my_variable now contains this string'
+
+name = "Sandra"
+greeting = "Hello #{{name}}! it's good to see you again"
+mission = "your mission, should you choose to accept it ... "
+
+puts "#{greeting} #{mission}"
+
+# Variables and Boolean Logic (together at last)
+
+my_number = 12
+my_number > 10
+# true
+
+my_number < 10
+# false
+
+your_number = 1
+
+my_number == your_number
+# false
+
+my_number != you_number
+# true
