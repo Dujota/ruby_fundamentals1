37d7bb31853debaeb84e6c37fcb1c9f6f5af8cc2
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-18 15:15:30 -0400

    added a simple gets program asking user for their name

--- excercise3.rb
+++ excercise3.rb
@@ -1,1 +1,12 @@
+# working with user input gets
+# gets = get string while puts = put string
+# gets asks for user input into the terminal, when they hit enter
+# gets returns that value into the vairable
+
+
 puts "What's your name?"
+user_name = gets
+puts "Hey there, #{user_name}"
+
+# gets vs gets.chomp
+# chomp removes unwanted whitespace from the end of your user's input. 
