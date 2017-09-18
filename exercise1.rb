8d02c0c534d2d08db55d8990d523f9825e3e3592
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-18 13:22:54 -0400

    added some multiplication and multi additions

--- exercise1.rb
+++ exercise1.rb
@@ -2,4 +2,9 @@ puts 2
 puts 3
 puts 2 + 3
 
-# puts 2 puts 3 puts 2 + 3 example of broken statements 
+# puts 2 puts 3 puts 2 + 3 example of broken statements
+
+puts 3 + 7 + 1
+
+puts 5 * 3 # this is a comment - multiplication uses the asterix (*) operator ,
+# not the letter x, new line of comment needs to be followed by a new # sign
