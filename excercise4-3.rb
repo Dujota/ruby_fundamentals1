2280b8d8ea118575cdb0d98c91c2797ebe8a36ba
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-18 16:23:54 -0400

    finished assignment 3

---
+++ excercise4-3.rb
@@ -0,0 +1,11 @@
+puts "enter your name here to see if we match :)"
+
+my_name = "denis"
+
+your_name = gets.chomp
+
+if my_name == your_name
+  puts "we have the same name -yay !!!!"
+else
+  puts "get a different name buddy!"
+end
