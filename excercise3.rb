fac6648246648ef9df31e2af5a00f158a408fa2e
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-18 15:42:41 -0400

    added comments for syntax on if statements

--- excercise3.rb
+++ excercise3.rb
@@ -15,3 +15,12 @@ puts "How old are you this year?"
 age = gets
 year_born = (2017 - age.to_i)
 puts "you are born in #{year_born}"
+
+# if statements:
+# if statements can be used to manage a program's "control flow", allowing you
+# to either execute or skip a block of code based on a condition that evaluates
+# to true or false
+# Syntax is:
+# if my_number > 1
+#   puts "The number is greater than 1"
+# end
